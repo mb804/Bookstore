75ecc6ee3e76b861da0cee7fff17f82a99b27580
mb <>
2022-02-27 05:47:09 +0200

    Harjoitus C4.4

--- Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
+import hh.swd20.Bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
 	@Autowired
-	private BookRepository repository;
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	// http://localhost:8080/index
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
@@ -25,7 +29,7 @@ public class BookController {
 	// http://localhost:8080/booklist
 	@RequestMapping(value = "/booklist")
 	public String bookList(Model model) {
-		model.addAttribute("books", repository.findAll());
+		model.addAttribute("books", bookRepository.findAll());
 		
 		return "booklist"; // booklist.html
 	}
@@ -34,6 +38,7 @@ public class BookController {
 	@RequestMapping(value = "/addbook")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
+		model.addAttribute("categories", categoryRepository.findAll());
 		
 		return "addbook"; // addbook.html
 	}
@@ -41,7 +46,7 @@ public class BookController {
 	// http://localhost:8080/save
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveBook(Book book) {
-		repository.save(book);
+		bookRepository.save(book);
 		
 		return "redirect:/booklist"; // return to booklist.html
 	}
@@ -49,7 +54,7 @@ public class BookController {
 	// http://localhost:8080/delete/{id}
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-		repository.deleteById(bookId);
+		bookRepository.deleteById(bookId);
 		
 		return "redirect:/booklist"; // return to booklist.html
 	}
@@ -57,7 +62,8 @@ public class BookController {
 	// http://localhost:8080/editbook/{id}
 	@RequestMapping(value = "/editbook/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model) {
-		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("book", bookRepository.findById(bookId));
+		model.addAttribute("categories", categoryRepository.findAll());
 		
 		return "/editbook"; // editbook.html
 	}
